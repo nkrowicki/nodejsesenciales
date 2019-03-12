aa7b335a8d373e42d246513e387e9a5cdbc1336c
nkrowicki <nahuelkrowicki@gmail.com>
2019-03-12 15:58:15 -0300

    clase 7

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require("express");
+const logger = require("morgan");
+const bodyParser = require("body-parser");
+
 const app = express();
+
+//Middleware
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 require("./routes/views")(app);
 require("./routes/special")(app);
 
 console.log("Iniciando Express.js");
-app.listen(3000, ()=>{
-    console.log("Express ha iniciado correctamente!");
+app.listen(3000, () => {
+  console.log("Express ha iniciado correctamente!");
 });
